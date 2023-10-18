b298105f4ab6dbe83e577bd633be83fcbb7238a6
SaraMagdy94 <engazharya@gmail.com>
2023-10-18 03:25:45 +0300

    s

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-iifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -7,14 +7,16 @@ iifndef MAIN_H
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdbool.h>
 
 extern char **environ;
+extern int last_exit_status;
 
 #define BUFFER_SIZE 1024
 
 void token(char *r, char *a[]);
 void execute(char *a[]);
+void execute_commands_interactive(char *commands[]);
 void execute_commands(char *commands[]);
 void remove_comment(char *buffer);
-void execute_commands_interactive(char *commands[])
 #endif /* MAIN_H */
